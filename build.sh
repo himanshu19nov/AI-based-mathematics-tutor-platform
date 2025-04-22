d679175409d6edd954f003603e6f80492f092f5d
Daniel <gygj.danielwong@gmail.com>
2025-04-22 17:18:21 +1000

    update build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -o errexit
 
 # Run migrations
-python manage.py migrate
+python manage.py migrate --noinput
 
 # Collect static files (optional if you're using Django's static hosting)
 python manage.py collectstatic --noinput
