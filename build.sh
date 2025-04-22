806b28ae9f182ddbd898f47c5f3e5201d08c3e60
Daniel <gygj.danielwong@gmail.com>
2025-04-22 17:00:04 +1000

    Add build.sh for Render deployment

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -o errexit
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files (optional if you're using Django's static hosting)
+python manage.py collectstatic --noinput
+
+# Start the Gunicorn server
+gunicorn myproject.wsgi:application --bind 0.0.0.0:$PORT
