a9a142828d19484de8073805cadfc6d6dd0704ab
Daniel <gygj.danielwong@gmail.com>
2025-04-23 15:27:17 +1000

    update settings.py for connecting frontend

--- myproject/settings.py
+++ myproject/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://mst-platform.onrender.com",
+    "https://mts-platform.onrender.com",
 ]
 
 CORS_ALLOW_HEADERS = [
